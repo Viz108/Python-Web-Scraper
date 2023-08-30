6c0f242a5f8a037f7f70866feb833aa1c756545e
Vishal S <vishalseeni@gmail.com>
2023-08-30 16:50:42 -0700

    Added synopsis scraping

--- scraper.py
+++ scraper.py
@@ -10,12 +10,19 @@ outputFile = open("output.txt", 'w', encoding="utf-8")
 outputFile.write(parsed_page.prettify())
 
 #Pull anime title
-title_tag = parsed_page.find('h1',class_="title-name")
-title = title_tag.string
-print(title)
+title = parsed_page.find('h1',class_="title-name").string
 
 #Pull score
+score = parsed_page.find('div', class_="score-label").string
 
 #Pull synopsis
+synopsis = parsed_page.find('p', itemprop="description").strings
+
 
 #Output
+print(title)
+print("\n")
+print(score)
+print("\n")
+for line in synopsis:
+    print(line)
